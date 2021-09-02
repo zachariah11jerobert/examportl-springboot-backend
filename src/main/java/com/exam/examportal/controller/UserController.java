6246d8fcae48ec6ee4d904e0e724d332b17e0b1d
unknown <zachariah11jerobert@gmail.com>
2021-09-02 15:47:18 +0530

    cross origin & profile string added

--- src/main/java/com/exam/examportal/controller/UserController.java
+++ src/main/java/com/exam/examportal/controller/UserController.java
@@ -12,6 +12,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -19,6 +20,7 @@ public class UserController {
     // creating user
     @PostMapping("/")
     public User createUser(@RequestBody User user) throws Exception {
+        user.setProfile("default.png");
         Set<UserRole> roles=new HashSet<>();
 
         Role role=new Role();
